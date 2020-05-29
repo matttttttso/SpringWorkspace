a68e06b6d6722647911f979baf07f79453ff665d
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-29 13:41:28 +0900

    Spring Sample ~5章まで(Hello~)

--- SpringSample/src/main/java/com/example/demo/trySpring/HelloRepository.java
+++ SpringSample/src/main/java/com/example/demo/trySpring/HelloRepository.java
@@ -8,20 +8,22 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class HelloRepository {
+	
 	// ポイント ２： JdbcTemplate  
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
 	public Map<String, Object> findOne(int id) {
 		
 		//SELECT文
 		String query = "SELECT"
-			+ "employee_id,"
-			+ "employee_name,"
-			+ "age"
+			+ " employee_id,"
+			+ " employee_name,"
+			+ " age "
 			+ "FROM employee"
 			+ "WHERE employee_id=?";
-
-		//検索 実行
+		
+		//検索実行
 		Map<String, Object> employee = jdbcTemplate.queryForMap(query, id);
 		return employee;
 	}
