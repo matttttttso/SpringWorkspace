17e9a324c68a04097908a6cb43f41d629be7b65f
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-29 13:41:28 +0900

    Spring Sample ~5章まで(Hello~)

--- SpringSample/src/main/java/com/example/demo/trySpring/HelloService.java
+++ SpringSample/src/main/java/com/example/demo/trySpring/HelloService.java
@@ -17,7 +17,7 @@ public class HelloService {
 		
 		// Mapから値を取得
 		int employeeId = (Integer)map.get("employee_id");
-		String employeeName = (String)map.get("employee_ name");
+		String employeeName = (String)map.get("employee_name");
 		int age = (Integer)map.get("age");
 		
 		// Employeeクラスに値をセット
