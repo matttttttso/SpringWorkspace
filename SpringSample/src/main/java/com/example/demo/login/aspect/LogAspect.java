61aadef45b0776c7672842fd5e121024afb55744
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-06-01 17:12:24 +0900

    SpringSample c7 AOP Around All method

--- SpringSample/src/main/java/com/example/demo/login/aspect/LogAspect.java
+++ SpringSample/src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -1,9 +1,8 @@
 package com.example.demo.login.aspect;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -17,13 +16,19 @@ public class LogAspect {
 	メソッド名：*（アスタリスク）を指定します。
 	引数：..（ドット２個）で全ての引数が対象になります。
 	*/
-	@Before("execution(* *..*.*Controller.*(..))")
-	public void startLog(JoinPoint jp) {
-		System.out.println("メソッド 開始： " + jp.getSignature());
-	}
-	
-	@After("execution(* *..*.*Controller.*(..))")
-	public void endLog(JoinPoint jp) {
-		System.out.println("メソッド 終了： " + jp.getSignature());
+	@Around("execution(* *..*.*Controller.*(..))")
+	public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+		
+		System.out.println("メソッド開始： " + jp.getSignature());
+
+		try {
+			Object Result = jp.proceed();
+			System.out.println("メソッド終了： " + jp.getSignature());
+			return Result;
+		} catch(Exception e) {
+			System.out.println("メソッド異常終了： " + jp.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
 	}
 }
