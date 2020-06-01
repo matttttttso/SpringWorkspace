5574471529b90a0b18162037f9aef5560a0c13dd
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-06-01 14:16:45 +0900

    SpringSample c6 Validation1 2020/6/1

--- SpringSample/src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ SpringSample/src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -17,6 +17,39 @@ import lombok.Data;
 @Data
 public class SignupForm {
 	
+	//パターン5
+	// 必須入力、メールアドレス形式
+	@NotBlank(message = "{require_check}")
+	@Email(message = "{email_check}")
+	private String userId; // ユーザーID
+	
+	// 必須入力、長さ4から100桁まで、半角英数字のみ
+	@NotBlank(message = "{require_check}")
+	@Length(min = 4, max = 100, message = "{length_check}")
+	@Pattern(regexp = "^[a-zA-Z0-9]+$", message = "{pattern_check}")
+	private String password; // パスワード
+	
+	// 必須入力
+	@NotBlank(message = "{require_check}")
+	private String userName; // ユーザー名
+	
+	// 必須入力
+	@NotNull(message = "{require_check}")
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date birthday; // 誕生日
+	
+	// 値が20から100まで
+	@Min(value = 20, message = "{min_check}")
+	@Max(value = 100, message = "{max_check}")
+	private int age; // 年齢
+	
+	// falseのみ可能
+	@AssertFalse(message = "{false_check}")
+	private boolean marriage; // 結婚ステータス
+	
+	/*
+	
+	//パターン1~4
 	// 必須入力、メールアドレス形式
 	@NotBlank
 	@Email
@@ -45,4 +78,6 @@ public class SignupForm {
 	// falseのみ可能
 	@AssertFalse
 	private boolean marriage; // 結婚ステータス
+	
+	*/
 }
