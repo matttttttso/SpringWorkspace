b388535204d66fc9479e2a3753984cf2559436b6
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-06-01 16:58:05 +0900

    SpringSample c7 AOP Before/After All method

--- SpringSample/src/main/java/com/example/demo/login/aspect/LogAspect.java
+++ SpringSample/src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -9,12 +9,20 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LogAspect {
-	@Before("execution(* com.example.demo.login.controller.LoginController.getLogin(..))")
+	//		"execution(<戻り値> <パッケージ名>.<クラス 名>.<メソッド名>(<引数>)"
+	/*
+	戻り値：*（アスタリスク）で全ての戻り値を指定しています。
+	パッケージ名：「*..*」と指定することで、全てのパッケージが対象となります。
+	クラス名：「*Controller」と指定することで、末尾にControllerと付くクラスが対象になります。
+	メソッド名：*（アスタリスク）を指定します。
+	引数：..（ドット２個）で全ての引数が対象になります。
+	*/
+	@Before("execution(* *..*.*Controller.*(..))")
 	public void startLog(JoinPoint jp) {
 		System.out.println("メソッド 開始： " + jp.getSignature());
 	}
 	
-	@After("execution(* com.example.demo.login.controller.LoginController.getLogin(..))")
+	@After("execution(* *..*.*Controller.*(..))")
 	public void endLog(JoinPoint jp) {
 		System.out.println("メソッド 終了： " + jp.getSignature());
 	}
