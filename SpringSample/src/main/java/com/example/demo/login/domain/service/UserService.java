23beebbe6780cb9347b03939323c8b28d10bef50
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-06-17 20:49:51 +0900

    SpringSample c8.2

--- SpringSample/src/main/java/com/example/demo/login/domain/service/UserService.java
+++ SpringSample/src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -1,5 +1,13 @@
 package com.example.demo.login.domain.service;
 
+import com.example.demo.login.domain.repository.UserDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
+  @Autowired
+  UserDao dao;
 }
