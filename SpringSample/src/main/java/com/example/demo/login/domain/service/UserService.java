6e68ecdcc243bbd4ccf4b05720e2e1c83e7406d4
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-06-18 18:57:08 +0900

    SpringSample c8.3 implement insert method in Service Class

--- SpringSample/src/main/java/com/example/demo/login/domain/service/UserService.java
+++ SpringSample/src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.login.domain.service;
 
+import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.repository.UserDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +11,18 @@ public class UserService {
 
   @Autowired
   UserDao dao;
+
+  // insert用メソッド
+  public boolean insert(User user) {
+    // insert実行
+    int rowNumber = dao.insertOne(user);
+    
+    // 判定用変数
+    boolean result = false;
+    if (rowNumber > 0) {
+      // insert成功
+      result = true;
+    }
+    return result;
+  }
 }
